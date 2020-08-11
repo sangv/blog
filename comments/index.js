95beed18c9277981d55175d1134ff4b4e790d677
Sang Venkatraman <sang@driveclutch.com>
2020-08-11 17:12:34 -0400

    working moderation

--- comments/index.js
+++ comments/index.js
@@ -21,19 +21,41 @@ app.post('/posts/:id/comments', async (req,res) => {
 
     
     comments.push({
-        id: commentId, content
+        id: commentId, content, status: 'pending'
     });
 
     commentsByPostId[req.params.id] = comments;
 
-    await axios.post('http://localhost:4005/events', {type: "CommentCreated", data: {id: commentId, content: content, postId: req.params.id}});
+    await axios.post('http://localhost:4005/events', {type: "CommentCreated", data: {id: commentId, content: content, postId: req.params.id, status: 'pending' }});
 
     res.status(201).send(comments);
 
 });
 
-app.post('/events', (req,res) => {
+app.post('/events', async (req,res) => {
     console.log(`${Date.now()}: Received Event`, req.body.type);
+
+    const {type, data } = req.body;
+
+    if (type === 'CommentModerated') {
+        const {postId, id, status, content} = data;
+
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id, 
+                status,
+                postId,
+                content
+            }
+        })
+    }
 
     res.send({});
 })
--- query/index.js
+++ query/index.js
@@ -20,12 +20,27 @@ app.post('/events', (req,res) => {
         posts[id] = {id, title, comments: []};
     }
     if (type === 'CommentCreated') {
-        const {id, content, postId} = data;
+        const {id, content, postId, status} = data;
 
         const post = posts[postId];
-        post.comments.push({id, content});
+        post.comments.push({id, content, status});
         
     }
+
+    if (type === 'CommentUpdated') {
+        
+        const {id, content, postId, status} = data;
+        const post = posts[postId];
+
+
+        const comment = post.comments.find(comment => {
+            return comment.id === id
+        });
+        comment.status = status; //saving in place so no need to push it back
+        comment.content = content; //not need imo
+        
+    }
+
     console.log(posts);
     
     res.send({});

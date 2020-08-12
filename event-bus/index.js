ff6057b98a49400bf2f22779f3f52bd3c9ebd1cd
Sang Venkatraman <sang@driveclutch.com>
2020-08-11 21:39:27 -0400

    their files

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,23 +1,29 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events',async (req,res) => {
-    const event = req.body;
-    
-    console.log(`About to send event ${event}`)
-    
-    await axios.post('http://localhost:4000/events',event);
-    await axios.post('http://localhost:4001/events',event);
-    await axios.post('http://localhost:4002/events',event);
-    await axios.post('http://localhost:4003/events',event);
+const events = [];
 
-    res.send({status: 'OK'});
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  events.push(event);
+
+  axios.post('http://localhost:4000/events', event);
+  axios.post('http://localhost:4001/events', event);
+  axios.post('http://localhost:4002/events', event);
+  axios.post('http://localhost:4003/events', event);
+
+  res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
-    console.log('Listening on 4005');
-})
+  console.log('Listening on 4005');
+});
--- moderation/index.js
+++ moderation/index.js
@@ -5,24 +5,26 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', async (req,res) => {
-    const {type, data} = req.body;
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
 
-    if (type === 'CommentCreated') {
-        const status = data.content.includes('orange') ? 'rejected' : 'approved';
-        console.log(`Inside commentCreated about to push data ${data}`);
-        await axios.post('http://localhost:4005/events', {
-            type: 'CommentModerated',
-            data: {
-               id: data.id,
-               postId: data.postId,
-               status,
-               content: data.content 
-            }
-        });
-    }
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
 
-    res.send({});
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content
+      }
+    });
+  }
+
+  res.send({});
 });
 
-app.listen(4003, () => {console.log("Moderation is listening on 4003");})
+app.listen(4003, () => {
+  console.log('Listening on 4003');
+});
